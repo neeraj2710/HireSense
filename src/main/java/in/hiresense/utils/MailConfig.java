321332d0c63e40399e91ddf3d343c0aa9973aad2
neera <neerajwadhwaney2003@gmail.com>
2025-09-23 16:10:17 +0530

    removed sensitive data updated gitignore

--- src/main/java/in/hiresense/utils/MailConfig.java
+++ src/main/java/in/hiresense/utils/MailConfig.java
@@ -1,14 +1,28 @@
 package in.hiresense.utils;
 
 import javax.mail.Session;
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.Properties;
 
 public class MailConfig {
 
-    private static final String USER_NAME = "elevarehire@gmail.com";
-    private static final String PASSWORD = "erqk bgts ufgz qrjl";
+    private static Properties loadProperties() {
+        Properties props = new Properties();
+        try (InputStream input = MailConfig.class.getClassLoader().getResourceAsStream("application.properties")) {
+            if (input != null) {
+                props.load(input);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return props;
+    }
 
     public static Session getSession(){
+        Properties config = loadProperties();
+        String username = config.getProperty("mail.username");
+        String password = config.getProperty("mail.password");
 
         Properties prop = new Properties();
         prop.put("mail.smtp.host","smtp.gmail.com");
@@ -17,8 +31,11 @@ public class MailConfig {
         prop.put("mail.smtp.socketFactory.port","465");
         prop.put("mail.smtp.socketFactory.class","javax.net.ssl.SSLSocketFactory");
 
-        MyAuthenticator auth = new MyAuthenticator(MailConfig.USER_NAME,MailConfig.PASSWORD);
+        MyAuthenticator auth = new MyAuthenticator(username,password);
         return Session.getInstance(prop,auth);
 
     }
+
+
+
 }
