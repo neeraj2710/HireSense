ac59b048741b6349c8450c219aa6b8a1982fe68b
neera <neerajwadhwaney2003@gmail.com>
2025-10-19 14:00:36 +0530

    Affinda API integration starts

---
+++ src/main/java/in/hiresense/utils/AffindaAPI.java
@@ -0,0 +1,10 @@
+package in.hiresense.utils;
+
+import java.io.File;
+import java.io.IOException;
+
+public class AffindaAPI {
+    public static String analyzeResume(File resumeFile) throws IOException{
+        return "";
+    }
+}
